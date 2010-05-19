c1feffb977f12ae41939631e694ddcff15e88d30
bgfarm <bgfarm@ebf3d73d-db81-0410-a8a2-c4a6456dd086>
2010-05-19 01:00:17 +0000

    Rich

---
+++ Rich/src/br/com/diegosilva/helloworld/Bean.java
@@ -0,0 +1,17 @@
+package br.com.diegosilva.helloworld;
+        
+
+public class Bean {
+	private String text;
+
+	public Bean() {
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
---
+++ Rich/work/org/apache/jsp/helloWorldRichFaces_jsp.java
@@ -0,0 +1,329 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class helloWorldRichFaces_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005ff_005fview;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fh_005fform;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005frich_005fpanel_0026_005fheader;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fh_005finputText_0026_005fvalue_005fsize;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fa4j_005fsupport_0026_005freRender_005fevent_005fnobody;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fh_005foutputText_0026_005fvalue_005fid_005fnobody;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _005fjspx_005ftagPool_005ff_005fview = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005fh_005fform = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005frich_005fpanel_0026_005fheader = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005fh_005finputText_0026_005fvalue_005fsize = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005fa4j_005fsupport_0026_005freRender_005fevent_005fnobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005fh_005foutputText_0026_005fvalue_005fid_005fnobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+    _005fjspx_005ftagPool_005ff_005fview.release();
+    _005fjspx_005ftagPool_005fh_005fform.release();
+    _005fjspx_005ftagPool_005frich_005fpanel_0026_005fheader.release();
+    _005fjspx_005ftagPool_005fh_005finputText_0026_005fvalue_005fsize.release();
+    _005fjspx_005ftagPool_005fa4j_005fsupport_0026_005freRender_005fevent_005fnobody.release();
+    _005fjspx_005ftagPool_005fh_005foutputText_0026_005fvalue_005fid_005fnobody.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<title>Hello World RichFaces</title>\r\n");
+      out.write("</head> \r\n");
+      out.write("<body>\r\n");
+      if (_jspx_meth_f_005fview_005f0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_f_005fview_005f0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  f:view
+    com.sun.faces.taglib.jsf_core.ViewTag _jspx_th_f_005fview_005f0 = new com.sun.faces.taglib.jsf_core.ViewTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_f_005fview_005f0);
+    _jspx_th_f_005fview_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_f_005fview_005f0.setParent(null);
+    _jspx_th_f_005fview_005f0.setJspId("jsp_608742525_0");
+    int _jspx_eval_f_005fview_005f0 = _jspx_th_f_005fview_005f0.doStartTag();
+    if (_jspx_eval_f_005fview_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      if (_jspx_eval_f_005fview_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.pushBody();
+        _jspx_th_f_005fview_005f0.setBodyContent((javax.servlet.jsp.tagext.BodyContent) out);
+        _jspx_th_f_005fview_005f0.doInitBody();
+      }
+      do {
+        out.write('\r');
+        out.write('\n');
+        out.write('	');
+        if (_jspx_meth_h_005fform_005f0(_jspx_th_f_005fview_005f0, _jspx_page_context))
+          return true;
+        out.write('\r');
+        out.write('\n');
+        int evalDoAfterBody = _jspx_th_f_005fview_005f0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+      if (_jspx_eval_f_005fview_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.popBody();
+      }
+    }
+    if (_jspx_th_f_005fview_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_f_005fview_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_f_005fview_005f0);
+      return true;
+    }
+    _jspx_th_f_005fview_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_f_005fview_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_h_005fform_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_f_005fview_005f0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  h:form
+    com.sun.faces.taglib.html_basic.FormTag _jspx_th_h_005fform_005f0 = new com.sun.faces.taglib.html_basic.FormTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_h_005fform_005f0);
+    _jspx_th_h_005fform_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_h_005fform_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_f_005fview_005f0);
+    _jspx_th_h_005fform_005f0.setJspId("jsp_608742525_1");
+    int _jspx_eval_h_005fform_005f0 = _jspx_th_h_005fform_005f0.doStartTag();
+    if (_jspx_eval_h_005fform_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      if (_jspx_eval_h_005fform_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.pushBody();
+        _jspx_th_h_005fform_005f0.setBodyContent((javax.servlet.jsp.tagext.BodyContent) out);
+        _jspx_th_h_005fform_005f0.doInitBody();
+      }
+      do {
+        out.write("\r\n");
+        out.write("\t\t");
+        if (_jspx_meth_rich_005fpanel_005f0(_jspx_th_h_005fform_005f0, _jspx_page_context))
+          return true;
+        out.write('\r');
+        out.write('\n');
+        out.write('	');
+        int evalDoAfterBody = _jspx_th_h_005fform_005f0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+      if (_jspx_eval_h_005fform_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.popBody();
+      }
+    }
+    if (_jspx_th_h_005fform_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_h_005fform_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005fform_005f0);
+      return true;
+    }
+    _jspx_th_h_005fform_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005fform_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_rich_005fpanel_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_h_005fform_005f0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  rich:panel
+    org.richfaces.taglib.PanelTag _jspx_th_rich_005fpanel_005f0 = new org.richfaces.taglib.PanelTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_rich_005fpanel_005f0);
+    _jspx_th_rich_005fpanel_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_rich_005fpanel_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_h_005fform_005f0);
+    // /helloWorldRichFaces.jsp(15,2) name = header type = javax.el.ValueExpression reqTime = false required = false fragment = false deferredValue = true expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
+    _jspx_th_rich_005fpanel_005f0.setHeader(new org.apache.jasper.el.JspValueExpression("/helloWorldRichFaces.jsp(15,2) 'Simples Impressão'",_el_expressionfactory.createValueExpression("Simples Impressão",java.lang.String.class)));
+    _jspx_th_rich_005fpanel_005f0.setJspId("jsp_608742525_2");
+    int _jspx_eval_rich_005fpanel_005f0 = _jspx_th_rich_005fpanel_005f0.doStartTag();
+    if (_jspx_eval_rich_005fpanel_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      if (_jspx_eval_rich_005fpanel_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.pushBody();
+        _jspx_th_rich_005fpanel_005f0.setBodyContent((javax.servlet.jsp.tagext.BodyContent) out);
+        _jspx_th_rich_005fpanel_005f0.doInitBody();
+      }
+      do {
+        out.write("\r\n");
+        out.write("\t\t\t");
+        if (_jspx_meth_h_005finputText_005f0(_jspx_th_rich_005fpanel_005f0, _jspx_page_context))
+          return true;
+        out.write("\r\n");
+        out.write("\t\t\t");
+        if (_jspx_meth_h_005foutputText_005f0(_jspx_th_rich_005fpanel_005f0, _jspx_page_context))
+          return true;
+        out.write("\r\n");
+        out.write("\t\t");
+        int evalDoAfterBody = _jspx_th_rich_005fpanel_005f0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+      if (_jspx_eval_rich_005fpanel_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.popBody();
+      }
+    }
+    if (_jspx_th_rich_005fpanel_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_rich_005fpanel_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_rich_005fpanel_005f0);
+      return true;
+    }
+    _jspx_th_rich_005fpanel_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_rich_005fpanel_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_h_005finputText_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_rich_005fpanel_005f0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  h:inputText
+    com.sun.faces.taglib.html_basic.InputTextTag _jspx_th_h_005finputText_005f0 = new com.sun.faces.taglib.html_basic.InputTextTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_h_005finputText_005f0);
+    _jspx_th_h_005finputText_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_h_005finputText_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_rich_005fpanel_005f0);
+    // /helloWorldRichFaces.jsp(16,3) name = size type = javax.el.ValueExpression reqTime = false required = false fragment = false deferredValue = true expectedTypeName = int deferredMethod = false methodSignature = null
+    _jspx_th_h_005finputText_005f0.setSize(new org.apache.jasper.el.JspValueExpression("/helloWorldRichFaces.jsp(16,3) '50'",_el_expressionfactory.createValueExpression("50",int.class)));
+    // /helloWorldRichFaces.jsp(16,3) name = value type = javax.el.ValueExpression reqTime = false required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
+    _jspx_th_h_005finputText_005f0.setValue(new org.apache.jasper.el.JspValueExpression("/helloWorldRichFaces.jsp(16,3) '#{bean.text}'",_el_expressionfactory.createValueExpression(_jspx_page_context.getELContext(),"#{bean.text}",java.lang.Object.class)));
+    _jspx_th_h_005finputText_005f0.setJspId("jsp_608742525_3");
+    int _jspx_eval_h_005finputText_005f0 = _jspx_th_h_005finputText_005f0.doStartTag();
+    if (_jspx_eval_h_005finputText_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      if (_jspx_eval_h_005finputText_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.pushBody();
+        _jspx_th_h_005finputText_005f0.setBodyContent((javax.servlet.jsp.tagext.BodyContent) out);
+        _jspx_th_h_005finputText_005f0.doInitBody();
+      }
+      do {
+        out.write("\r\n");
+        out.write("\t\t\t\t");
+        if (_jspx_meth_a4j_005fsupport_005f0(_jspx_th_h_005finputText_005f0, _jspx_page_context))
+          return true;
+        out.write("\r\n");
+        out.write("\t\t\t");
+        int evalDoAfterBody = _jspx_th_h_005finputText_005f0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+      if (_jspx_eval_h_005finputText_005f0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+        out = _jspx_page_context.popBody();
+      }
+    }
+    if (_jspx_th_h_005finputText_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_h_005finputText_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005finputText_005f0);
+      return true;
+    }
+    _jspx_th_h_005finputText_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005finputText_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_a4j_005fsupport_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_h_005finputText_005f0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  a4j:support
+    org.ajax4jsf.taglib.html.jsp.AjaxSupport _jspx_th_a4j_005fsupport_005f0 = new org.ajax4jsf.taglib.html.jsp.AjaxSupport();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_a4j_005fsupport_005f0);
+    _jspx_th_a4j_005fsupport_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_a4j_005fsupport_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_h_005finputText_005f0);
+    // /helloWorldRichFaces.jsp(17,4) name = event type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_a4j_005fsupport_005f0.setEvent("onkeyup");
+    // /helloWorldRichFaces.jsp(17,4) name = reRender type = javax.el.ValueExpression reqTime = false required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
+    _jspx_th_a4j_005fsupport_005f0.setReRender(new org.apache.jasper.el.JspValueExpression("/helloWorldRichFaces.jsp(17,4) 'rep'",_el_expressionfactory.createValueExpression("rep",java.lang.Object.class)));
+    _jspx_th_a4j_005fsupport_005f0.setJspId("jsp_608742525_4");
+    int _jspx_eval_a4j_005fsupport_005f0 = _jspx_th_a4j_005fsupport_005f0.doStartTag();
+    if (_jspx_th_a4j_005fsupport_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_a4j_005fsupport_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_a4j_005fsupport_005f0);
+      return true;
+    }
+    _jspx_th_a4j_005fsupport_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_a4j_005fsupport_005f0);
+    return false;
+  }
+
+  private boolean _jspx_meth_h_005foutputText_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_rich_005fpanel_005f0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  h:outputText
+    com.sun.faces.taglib.html_basic.OutputTextTag _jspx_th_h_005foutputText_005f0 = new com.sun.faces.taglib.html_basic.OutputTextTag();
+    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_h_005foutputText_005f0);
+    _jspx_th_h_005foutputText_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_h_005foutputText_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_rich_005fpanel_005f0);
+    // /helloWorldRichFaces.jsp(19,3) name = value type = javax.el.ValueExpression reqTime = false required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
+    _jspx_th_h_005foutputText_005f0.setValue(new org.apache.jasper.el.JspValueExpression("/helloWorldRichFaces.jsp(19,3) '#{bean.text}'",_el_expressionfactory.createValueExpression(_jspx_page_context.getELContext(),"#{bean.text}",java.lang.Object.class)));
+    // /helloWorldRichFaces.jsp(19,3) name = id type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_h_005foutputText_005f0.setId("rep");
+    _jspx_th_h_005foutputText_005f0.setJspId("jsp_608742525_5");
+    int _jspx_eval_h_005foutputText_005f0 = _jspx_th_h_005foutputText_005f0.doStartTag();
+    if (_jspx_th_h_005foutputText_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_th_h_005foutputText_005f0.release();
+      org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005foutputText_005f0);
+      return true;
+    }
+    _jspx_th_h_005foutputText_005f0.release();
+    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_h_005foutputText_005f0);
+    return false;
+  }
+}
